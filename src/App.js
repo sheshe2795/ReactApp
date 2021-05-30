a58c884656cd65aaef4cb20e515edc622b6f92d4
Shelique <81508244+sheshe2795@users.noreply.github.com>
2021-05-30 23:02:42 +0900

    Revert "Profile Card"
    This reverts commit 3a2c5ff99d56c51848c27c7d890fc7aedc49e118.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import {useState} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Display from './components/Display';
-import Counter from './components/Counter';
-import logo from './sheli2.jpg';
 
 function App() {
-
-  const [skills, setSkills] = useState("My name is Shelique")
-  
-  const handleName =()=>{
-    setSkills("My name is Shelique")
-  }
-  const handleSkills =()=>{
-    setSkills("I am a Frontend Developer")
-  }
-  ;
-
-
   return (
-    <>
-    <div className= "main">
-      <img className="sheli" src={logo} alt="profile"></img>
-      
-      <Display name = {skills}/>
-        <div>
-         <button className= "btn" onClick= {handleName}>Name</button>
-          <button className= "btn2" onClick= {handleSkills}>Skills</button>
-        </div>
-        
-       <Counter/> 
-      
-      <button className="github"> <a href="https://github.com/sheshe2795" target="_blank" alt="github profile"><i class="fab fa-github fa-3x"></i></a> </button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-   </>   
-
   );
 }
 
